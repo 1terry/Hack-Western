41fcc2c6afa50d8ec917237c3e72dc7c8432a6a4
Kevin Yang <kyang267@uwo.ca>
2021-11-19 23:35:03 -0500

    November 19, 2021 (11:34 pm)
    Created app class and made changes for flask integration.

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 """
 This is the main class. It will host a local flask server and be responsible for bridging the gap between the frontend and the backend.
+
+November 19, 2021
+- Created app class
+
+Good resources:
+https://stackoverflow.com/questions/22947905/flask-example-with-post
+
 """
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify 
@@ -13,8 +20,24 @@ CORS(app)
 def home():
     return ("<h1>Welcome to the server</h1>")
 
-@app.route('/api/', methods = ['POST'])
+@app.route('/api/', methods = ['ACCOUNT', 'MODIFYACCOUNT', 'ADDMEAL', 'MODIFYMEAL'])
 def postmethod():
+    
+
+    # If the request is an account creation.
+    if request.method == 'ACCOUNT':
+
+
+    # If the server wants to change something about the account
+    elif request.method == 'MODIFYACCOUNT':
+
+
+    # If the server wants to add a meal
+    elif request.method == 'ADDMEAL':
+    
+
+    # If the server wants to modify a meal
+    elif request.method == 'MODIFYMEAL'
 
     # We will need to get stuff from the frontend.
 
