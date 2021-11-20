2926c293c7d288924351f2de4d3458dc255a2cda
kyasui <kyasui@uwo.ca>
2021-11-20 13:54:51 -0500

    Worked on API

--- API.py
+++ API.py
@@ -1,55 +1,53 @@
-
 class API:
-    nutrient_dictionary = {}
+
     def __init__(self):
         import http.client
-        self.conn = http.client.HTTPSConnection("edamam-edamam-nutrition-analysis.p.rapidapi.com")
+        self.conn = http.client.HTTPSConnection("nutritionix-api.p.rapidapi.com")
 
         self.headers = {
-            'x-rapidapi-host': "edamam-edamam-nutrition-analysis.p.rapidapi.com",
+            'x-rapidapi-host': "nutritionix-api.p.rapidapi.com",
             'x-rapidapi-key': "83a686583amsh4b5dd474b143968p1989f2jsnf3a850b88cd6"
         }
 
-    def call_api(self, count, unit, item_name):
-        data = "/api/nutrition-data?ingr=" + count + "%20" + unit + "%20" + item_name
-        self.conn.request("GET", data, headers=self.headers)
+    def call_api(self, item_name):
+
+        split_name = item_name.split(' ')
+        item_string = ''
+        start = True
+        for i in split_name:
+            if start:
+                item_string = i
+                start = False
+            else:
+                item_string = item_string + "%20" + i
+
+        item_string = "/v1_1/search/" + item_string + "=?fields=item_name%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_saturated_fat%2Cnf_total_fat%2Cnf_trans_fatty_acid%2Cnf_total_carbohydrate%2Cnf_dietary_fiber%2Cnf_sugars%2Cnf_protein%2Cnf_cholesterol%2C%nf_calcium_dv%2Cnf_iron_dv%2Cnf_vitamin_a_dv%2Cnf_vitamin_c_dv%2Cnf_potassium"
+        self.conn.request("GET", item_string, headers=self.headers)
         res = self.conn.getresponse()
         data = res.read()
+        data_list = data.decode("utf-8").split("{")
+        index = 3;
 
-        data_list = data.decode("utf-8").split("}")
+        list_of_output = []
+        while index != len(data_list) - 1:
+            list_of_output.append(self.process_line(data_list[index]))
+            index += 2
 
-        cal = ''
-        index = data_list[0].find('calories') + 10
+        return list_of_output
 
-        while data_list[0][index] != ",":
-            cal = cal + data_list[0][index]
+    def process_line(self, data_line):
+        nutrient_dictionary = {}
+        name = data_line[0:data_line.find("brand") - 2].replace('"', '')
+        rest = data_line[data_line.find("brand") - 1:len(data_line) - 1].replace('}', '').replace('"', '')
+
+        name_list = name.split(':')
+        nutrient_dictionary[name_list[0]] = name_list[1]
+
+        index = 0
+        info_list = rest.split(',')
+        while index != len(info_list):
+            nutrient_info = info_list[index].split(':')
+            nutrient_dictionary[nutrient_info[0]] = nutrient_info[1]
             index += 1
 
-        self.nutrient_dictionary['calories'] = cal
-
-        for i in range(1, len(data_list)-3):
-            if (data_list[i]) != "":
-                index = data_list[i].find('label')+8
-                nutrient = ''
-                while data_list[i][index] != "\"":
-                    nutrient = nutrient + data_list[i][index]
-                    index += 1
-
-                index = data_list[i].find('quantity') + 10
-                value = ''
-                while data_list[i][index] != ",":
-                    value = value + data_list[i][index]
-                    index += 1
-                index = data_list[i].find('unit') + 7
-                while data_list[i][index] != "\"":
-                    value = value + data_list[i][index]
-                    index += 1
-
-            self.nutrient_dictionary[nutrient] = value
-
-        return self.nutrient_dictionary
-
-
-
-
-
+        return nutrient_dictionary
