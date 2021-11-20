1d04b5350cbc27af23ae7fe1213cf5b1cc81b148
kyasui <kyasui@uwo.ca>
2021-11-19 23:39:48 -0500

    Worked on API

--- API.py
+++ API.py
@@ -1,6 +1,6 @@
 
 class API:
-
+    dict = {}
     def __init__(self):
         import http.client
         self.conn = http.client.HTTPSConnection("edamam-edamam-nutrition-analysis.p.rapidapi.com")
@@ -16,7 +16,33 @@ class API:
         res = self.conn.getresponse()
         data = res.read()
 
-        print(data.decode("utf-8"))
+        data_list = data.decode("utf-8").split("}")
+        # code for calories and attributes
+
+        for i in range(1, len(data_list)-3):
+            if (data_list[i]) != "":
+                index = data_list[i].find('label')+8
+                nutrient = ''
+                while data_list[i][index] != "\"":
+                    nutrient = nutrient + data_list[i][index]
+                    index += 1
+                print(nutrient)
+
+                index = data_list[i].find('quantity') + 10
+                value = ''
+                while data_list[i][index] != ",":
+                    value = value + data_list[i][index]
+                    index += 1
+                index = data_list[i].find('unit') + 7
+                while data_list[i][index] != "\"":
+                    value = value + data_list[i][index]
+                    index += 1
+                print(value)
+
+
+api = API()
+api.call_api("1", "tablespoon", "honey")
+
 
 
 
