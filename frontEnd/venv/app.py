0cc5f993620e8b0a866cb3590a724cee4791260a
Terry <t.juottawa@gmail.com>
2021-11-21 03:21:04 -0500

    added dropdown

--- frontEnd/venv/app.py
+++ frontEnd/venv/app.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, url_for
 from person import person
 from API import API
 from FoodItem import FoodItem
+from foodList import foodList
 from meal import meal
 
 app = Flask (__name__)
@@ -9,7 +10,46 @@ app = Flask (__name__)
 
 @app.route ("/")
 def home():
-    return render_template('testForm.html')
+    return render_template('index.html', 
+    data=[{'name':'little'},{'name':'light'},{'name':'moderate'},{'name':'hard'},{'name':'work'},{'name':'athlete'}])
+    
+
+@app.route('/toList', methods = ['GET', 'POST'])
+def getList():
+    if request.method == 'GET':
+       print("Should never GET")
+    if request.method == 'POST':
+       form_data = request.form
+                                        
+@app.route('/foodData', methods = ['GET', 'POST'])
+def foodData():
+    if request.method == 'GET':
+       print("Should never GET")
+    if request.method == 'POST':
+        foods = []
+        foodNames = []
+        form_data = request.form
+        count = 0
+        for key,value in form_data.items():
+            if (count == 0):
+                food_list = foodList(value)
+                foods = food_list.return_list()
+                for i in foods:
+                    brandName = i.get_brand()
+                    itemName = i.get_name()
+                    foodName = itemName + ", brand: " + brandName
+                    foodNames.append(foodName)
+                    
+                # print("filler")
+           
+                # return_dictionary ["Food"] = value
+            count = count + 1
+
+        
+
+
+        return render_template('index.html', form_data = foodNames, scrollToAnchor='end')
+    
 
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
@@ -31,35 +71,42 @@ def data():
         exercise_level = "NO"
 
         # This will be for if the user wishes to bulk, maintain, or lose weight
-        condition = ""
+        condition = "losing"
 
         # Creating values for the user.
         for key,value in form_data.items():
             
             if counter == 0:
-                sex = value
-                return_dictionary ["Sex"] = sex
-            
-            elif counter == 1:
-                height = int(value) 
-                return_dictionary ["Height"] = height
-            
-            elif counter == 2:
-                weight = int(value)
-                return_dictionary ["Weight"] = weight
- 
-            elif counter == 3:
                 age = int(value)
                 return_dictionary ["Age"] = age
+               
+            
+            elif counter == 1:
+                print(request.form['gender'])
+                sex = value
+                return_dictionary ["Sex"] = sex
+                
+            
+            elif counter == 2:
+                height = int(value) 
+                return_dictionary ["Height"] = height
+               
+ 
+            elif counter == 3:
+                weight = int(value)
+                return_dictionary ["Weight"] = weight
 
             elif counter == 4:
-                exercise_level = value
+                exercise = request.form.get('exercise')
+                exercise_level = exercise
                 return_dictionary ["Exercise Level"] = exercise_level
 
             elif counter == 5:
                 condition = value
+                return_dictionary ["Condition"] = condition
             counter = counter + 1
     
+        print("number of items: ", counter)
         # Create a temporary person with the given attributes 
         Kohei = person(sex, height, weight, age, exercise_level)
 
@@ -69,7 +116,7 @@ def data():
         elif (condition.lower() == "bulk"):
             return_dictionary ["Condition"] = Kohei.return_bulking()
         elif (condition.lower() == "losing"):
-            return_dictionary ["Condition"] = Kohei.return_losing("moderate")
+            return_dictionary ["Maintenance Calories"] = Kohei.return_losing("moderate")
 
         # Returns a dictionary 
         return render_template('data.html', form_data = return_dictionary)
